05599bfa40663feaf6b80a25ec57671166d530f5
Sayali Alatkar <sayali7.alatkar@gmail.com>
2023-11-25 10:21:48 -0600

    Update varPar.R

--- VariancePartition/varPar.R
+++ VariancePartition/varPar.R
@@ -4,7 +4,7 @@ library(ggplot2)
 # load simulated data:
 # geneExpr: matrix of gene expression values
 # info: information/metadata about each sample
-#load("/media/sayali/Extreme SSD/My_Work/psychAD/iBrainMap/VariancePartition/tutorial/varPartData.rda")
+#load("/VariancePartition/tutorial/varPartData.rda")
 
 #form <- ~ Age + (1|Individual) + (1|Tissue) + (1|Batch)
 #varPart <- fitExtractVarPartModel( geneExpr, form, info )
@@ -14,13 +14,13 @@ library(ggplot2)
 #plotVarPart( vp )
 
 ##################################### START HERE #########################
-labels <- read.csv("/media/sayali/Extreme SSD/My_Work/psychAD/iBrainMap/VariancePartition/input_data/labels_Micro_allpatients_forvarpar.csv")
+labels <- read.csv("/VariancePartition/input_data/labels.csv")
 labels$AD <- as.character(labels$AD)
 labels$SCZ <- as.character(labels$SCZ)
 #labels$SubID <- as.factor(labels$SubID)
 labels$PLAQUE <- replace(labels$PLAQUE, is.na(labels$PLAQUE), 0)
 
-ct_edges <- read.csv("/media/sayali/Extreme SSD/My_Work/psychAD/iBrainMap/VariancePartition/input_data/ct-specific_tf-tg-links_allpatients_Micro_forvarpar.csv")
+ct_edges <- read.csv("/VariancePartition/input_data/ct-specific_tf-tg-links.csv")
 ct_edges <- as.data.frame(ct_edges)
 ct_edges_row <- ct_edges$X
 ct_edges<-ct_edges[,-c(1)]
@@ -38,10 +38,10 @@ vp_1 <- sortCols( varPart_1 )
 plotPercentBars( vp_1[1:20,] )
 plotVarPart( vp_1 )
 
-fig1 <- plotVarPart( vp_1 , label.angle = 45, main = "Microglia: TF-TG links", )
-ggsave("/media/sayali/Extreme SSD/My_Work/psychAD/iBrainMap/VariancePartition/plots/ct-specific_tf-tg-links_allpatients_Micro_violin.png", fig1, dpi=300, width = 4, height = 3,)
+fig1 <- plotVarPart( vp_1 , label.angle = 45, main = "ct: TF-TG links", )
+ggsave("/VariancePartition/plots/ct-specific_tf-tg-links_violin.png", fig1, dpi=300, width = 4, height = 3,)
 
 fig2 <- plotPercentBars( vp_1[1:20,] )
-ggsave("/media/sayali/Extreme SSD/My_Work/psychAD/iBrainMap/VariancePartition/plots/ct-specific_ct-ct-links_allpatients_VarPar_bar.png", fig2, dpi=300, width = 3, height = 3,)
+ggsave("/VariancePartition/plots/ct-specific_tf-tg-links_VarPar_bar.png", fig2, dpi=300, width = 3, height = 3,)
 
-write.csv(vp_1, file = "/media/sayali/Extreme SSD/My_Work/psychAD/iBrainMap/VariancePartition/plots/ct-specific_ct-ct-links_allpatients_VarPar.csv")
+write.csv(vp_1, file = "/VariancePartition/plots/ct-specific_tf-tg-links_VarPar.csv")
